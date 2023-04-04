f046e5332fa84a8922fa1f4c3ffe70d71f58ea43
beatrice <helgapaul389@gmail.com>
2023-04-04 21:53:36 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-function isPalindrome(word) {
+
+  function isPalindrome(s) {
+    // Write your algorithm here
+
+    // Convert the string to lowercase and remove all non-alphanumeric characters
+    s = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+  
+    // Check if the string is equal to its reverse
+    return s === s.split("").reverse().join("");
+  }
+  
   // Write your algorithm here
-}
 
 /* 
   Add your pseudocode here
+  create a function is palindrome
+  the function should receive a parameter
+  the parameter should be a string
+  the function should return a boolean
+  the function should check if the string is a palindrome
+  the function should return true if the string is a palindrome
+  the function should return false if the string is not a palindrome
+
 */
 
 /*
